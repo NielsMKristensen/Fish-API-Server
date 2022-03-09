da6c4f0c54177574815f06a15e2d6921b22b4aef
NielsMKristensen <Niels.Kristensen@Hotmail.com>
2022-03-09 12:09:41 +0100

    testing deployment

--- config/index.js
+++ config/index.js
@@ -22,8 +22,9 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      //origin: process.env.ORIGIN || "http://localhost:3000",
+      origin: process.env.ORIGIN,
+      credentials: true
     })
   );
 
